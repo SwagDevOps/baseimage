13fa12b11d8a73ce73d1048f5423b1923a4357a4
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-07-04 22:46:33 +0200

    color_prompt (profile) introduced

---
+++ image/files/etc/profile.d/color_prompt.sh
@@ -0,0 +1,10 @@
+# Setup a red prompt for root and a green one for users.
+
+NORMAL="\[\e[0m\]"
+RED="\[\e[1;31m\]"
+GREEN="\[\e[1;32m\]"
+
+case "$(whoami)" in
+    root) PS1="$RED\h [$NORMAL\w$RED]# $NORMAL" ;;
+    *)    PS1="$GREEN\h [$NORMAL\w$GREEN]\$ $NORMAL" ::
+esac
