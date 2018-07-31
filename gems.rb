088ca7ce6b3b3baef874716aeb2ef60d83e7fc29
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-07-31 22:02:06 +0200

    serverspec (gems) added

--- gems.rb
+++ gems.rb
@@ -9,7 +9,14 @@ group :default do
   gem 'kamaze-docker_image', \
       github: 'SwagDevOps/kamaze-docker_image', branch: 'master'
 
+  gem 'kamaze-project', '~> 1.0', '>= 1.0.3'
   gem 'rake', '~> 12.3'
   gem 'sys-proc', '~> 1.1', '>= 1.1.2'
   gem 'vendorer', '~> 0.1'
+end
+
+group :test do
+  gem 'fuubar', '~> 2.3'
+  gem 'rspec', '~> 3.7'
+  gem 'serverspec', '~> 2.41'
 end
