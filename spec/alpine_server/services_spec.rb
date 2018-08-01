6c4cb37a16878e68f3a264d911709eaac94f7bbe
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-08-01 09:12:35 +0200

    services_spec (spec) sv used to ensure service are running + rewrite

--- spec/alpine_server/services_spec.rb
+++ spec/alpine_server/services_spec.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
 describe 'services', :services do
-  describe command('ps -Ao args --no-headers') do
-    ['/usr/sbin/crond -S -f',
-     '/sbin/syslogd -n -D',
-     '/usr/sbin/dropbear -Fsp 22 -r /etc/dropbear/host_rsa'].each do |cmd|
-      its(:stdout) { should match(/^#{cmd}$/) }
+  {
+    cron: '/usr/sbin/crond -S -f',
+    syslog: '/sbin/syslogd -n -D',
+    ssh: '/usr/sbin/dropbear -Fsp 22 -r /etc/dropbear/host_rsa'
+  }.each do |service, command|
+    describe command('ps -Ao args --no-headers') do
+      its(:stdout) { should match(/^#{command}$/) }
+    end
+
+    describe command("bash -lc '/sbin/sv status #{service}'") do
+      its(:stdout) { should match(/^run:\s+#{service}:/) }
     end
   end
 end
