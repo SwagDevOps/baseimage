b7bdffe042ee4927b9c6d631f08d52162f97a785
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-08-01 08:06:31 +0200

    spec_helper/serverspec (spec) path added + minor additions

--- spec/spec_helper/serverspec.rb
+++ spec/spec_helper/serverspec.rb
@@ -4,15 +4,16 @@ require 'serverspec'
 require 'net/ssh'
 require_relative 'image'
 
-set :backend, :ssh
-
 image.ssh_runner.wait.network.fetch(0).tap do |host|
   options = {
     user_known_hosts_file: '/dev/null',
     user: 'root'
   }.merge(Net::SSH::Config.for(host))
 
+  set :backend, :ssh
+  set :os, family: :alpine
   set :host,         host
   set :ssh_options,  options
   set :disable_sudo, true
+  set :path, '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 end
