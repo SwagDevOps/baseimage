31a37ea41bf533892514bd4e5e80bbe0695a7fa8
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-08-01 05:21:38 +0200

    gems refactor

--- gems.rb
+++ gems.rb
@@ -9,18 +9,17 @@ group :default do
   gem 'kamaze-docker_image', \
       github: 'SwagDevOps/kamaze-docker_image', branch: 'master'
 
-  gem 'kamaze-project', '~> 1.0', '>= 1.0.3'
   gem 'rake', '~> 12.3'
   gem 'sys-proc', '~> 1.1', '>= 1.1.2'
   gem 'vendorer', '~> 0.1'
 end
 
 group :development do
+  gem 'kamaze-project', '~> 1.0', '>= 1.0.3'
   gem 'rubocop', '~> 0.56'
 end
 
 group :test do
-  gem 'fuubar', '~> 2.3'
   gem 'rspec', '~> 3.7'
   gem 'serverspec', '~> 2.41'
 end
