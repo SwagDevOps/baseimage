58d8334baf5f3103c106211b503bde217069e0b4
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-08-01 09:28:38 +0200

    etc_spec (spec) /etc/profile.d/environment.sh + /etc/environment added

--- spec/alpine_server/etc_spec.rb
+++ spec/alpine_server/etc_spec.rb
@@ -5,4 +5,16 @@ describe 'etc contents', :etc do
     it { should be_file }
     its(:content) { should match(/^3.8.[0-9]+$/) }
   end
+
+  describe file('/etc/profile.d/environment.sh') do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_mode 644 }
+  end
+
+  describe file('/etc/environment') do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_mode 644 }
+  end
 end
