051cf54de33687dcfd02cb6fdc4c766329bb937a
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-08-02 23:30:30 +0200

    dumb-init update to v1.2.2

--- spec/alpine_server/init_spec.rb
+++ spec/alpine_server/init_spec.rb
@@ -1,16 +1,19 @@
 # frozen_string_literal: true
 
 describe 'init', :init do
+  let(:version) { '1.2.2' }
+  let(:md5) { '79f195aae73dda5424c9578918ca2623' }
+
   describe file('/sbin/init') do
     it { should be_file }
     it { should be_owned_by 'root' }
     it { should be_grouped_into 'root' }
     it { should be_mode 775 }
 
-    its(:md5sum) { should eq('7dba581e723e379c588d67a6b2b83b2a') }
+    its(:md5sum) { should eq(md5) }
   end
 
   describe command('/sbin/init --version 2>&1') do
-    its(:stdout) { should match(/^dumb-init v1.2.1$/) }
+    its(:stdout) { should match(/^dumb-init v#{version}$/) }
   end
 end
