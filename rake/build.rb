a2957033fd3ce006f54f590e93f07c12b0dc3f54
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-08-04 18:43:04 +0200

    Vendorfile -> image/Vendorfile

--- rake/build.rb
+++ rake/build.rb
@@ -6,7 +6,7 @@ autoload :Tenjin, 'tenjin'
 
 task default: [:build]
 
-task 'image/Dockerfile' do |task|
+task "#{image.path}/Dockerfile" do |task|
   image.version.to_h.merge(image: image).tap do |context|
     Tenjin::Engine
       .new(cache: false)
@@ -15,16 +15,19 @@ task 'image/Dockerfile' do |task|
   end
 end
 
-task 'image/files/build/vendor' do
-  Vendorer.new(update: false).tap do |v|
-    config_locations = ['Vendorfile.rb', 'Vendorfile']
-    config_location = config_locations.detect { |f| File.exist?(f) }
+task "#{image.path}/files/build/vendor" do |task|
+  Dir.chdir(image.path) do
+    Vendorer.new(update: false).tap do |v|
+      config_locations = ['Vendorfile.rb', 'Vendorfile']
+      config_location = config_locations.detect { |f| File.exist?(f) }
 
-    v.parse(File.read(config_location || config.locations.last))
+      v.parse(File.read(config_location || config_locations.last))
+    end
   end
 end
 
-['image/files/build/vendor', 'image/Dockerfile'].each do |name|
+["#{image.path}/Dockerfile",
+ "#{image.path}/files/build/vendor"].each do |name|
   CLOBBER.push(name)
 
   task 'pre_build' do
