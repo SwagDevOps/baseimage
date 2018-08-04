5e364daf0515d28f230e1321abace0c3126ac330
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-08-04 19:14:27 +0200

    services_spec (spec) examples added for BusyBox binaries

--- spec/alpine_server/services_spec.rb
+++ spec/alpine_server/services_spec.rb
@@ -15,3 +15,13 @@ describe 'services', :services do
     end
   end
 end
+
+describe 'services', :services do
+  # This services SHOULD be provided by BusyBox
+  ['/sbin/syslogd',
+   '/usr/sbin/crond'].each do |binary|
+    describe command("#{binary} --help 2>&1") do
+      its(:stdout) { should match(/^BusyBox v[0-9]+.[0-9 \.]+/) }
+    end
+  end
+end
