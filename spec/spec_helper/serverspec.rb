b5e7c042ff23c7b7c5ff3780b87ef22ba37889bc
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-08-05 16:54:10 +0200

    spec_helper/serverspec (spec) image.ssh values use

--- spec/spec_helper/serverspec.rb
+++ spec/spec_helper/serverspec.rb
@@ -12,6 +12,7 @@ image.ssh.wait.network.fetch(0).tap do |host|
   set :path, '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
   set :ssh_options, Net::SSH::Config.for(host).merge(
     user_known_hosts_file: '/dev/null',
-    user: 'root'
+    user: image.ssh.fetch(:user),
+    port: image.ssh.fetch(:port)
   )
 end
