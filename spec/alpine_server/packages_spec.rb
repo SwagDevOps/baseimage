1f0526095dcbcf04aaa4f610452e5d8ee5034cbd
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-08-07 18:21:35 +0200

    packages_spec (spec) update

--- spec/alpine_server/packages_spec.rb
+++ spec/alpine_server/packages_spec.rb
@@ -4,13 +4,14 @@
 #       because it seems too permissive (lacks of efficiency).
 describe 'installed packages', :packages do
   [
-    %w[bash bash-completion htop screen],
+    %w[bash bash-completion htop multitail screen],
     %w[runit busybox file tzdata],
     %w[curl sed tar grep shadow pwgen rsync],
     %w[vim less coreutils sed procps],
     %w[dropbear dropbear-convert dropbear-scp],
     %w[ruby ruby-bundler],
     %w[ruby-bigdecimal ruby-etc ruby-fiddle ruby-json],
+    %w[make gcc musl-dev],
   ].flatten.each do |package|
     describe command("/sbin/apk info -e #{package}") do
       its(:stdout) { should match(/^#{package}$/) }
