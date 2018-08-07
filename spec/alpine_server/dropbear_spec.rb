c131f33413d059300660c1b5639c6719316ac594
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-08-07 18:34:59 +0200

    dropbear_spec (spec) introduced

---
+++ spec/alpine_server/dropbear_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+describe 'dropbear', :dropbear do
+  {
+    dropbear: '/usr/sbin/dropbear',
+    # This executables are executed during dropbear setup (init)
+    dropbearconvert: '/usr/bin/dropbearconvert',
+    dropbearkey: '/usr/bin/dropbearkey',
+  }.each do |k, file|
+    describe file(file) do
+      it { should be_file }
+      it { should be_owned_by 'root' }
+      it { should be_grouped_into 'root' }
+      it { should be_mode 755 }
+    end
+
+    describe command("/usr/bin/which #{k}") do
+      its(:stdout) { should match(/^#{file}$/) }
+    end
+  end
+end
