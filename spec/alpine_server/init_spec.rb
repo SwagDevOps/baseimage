eed6a6a2824ed4b3952a0e8d8ed0e89e5537e103
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-08-07 13:28:33 +0200

    init (build) dumb-init built from sources

--- spec/alpine_server/init_spec.rb
+++ spec/alpine_server/init_spec.rb
@@ -2,15 +2,17 @@
 
 describe 'init', :init do
   let(:version) { '1.2.2' }
-  let(:md5) { '79f195aae73dda5424c9578918ca2623' }
+  let(:md5) { '5cc73adacb140d682c73082ba1329960' } # x86_64
 
   describe file('/sbin/init') do
     it { should be_file }
     it { should be_owned_by 'root' }
     it { should be_grouped_into 'root' }
-    it { should be_mode 775 }
+    it { should be_mode 755 }
 
-    its(:md5sum) { should eq(md5) }
+    if 'x86_64' == RbConfig::CONFIG['host_cpu']
+      its(:md5sum) { should eq(md5) }
+    end
   end
 
   describe command('/sbin/init --version 2>&1') do
