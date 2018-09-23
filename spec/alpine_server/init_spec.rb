0717048bedeb99e8cc7843da472fdf4babbf3283
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-09-24 00:14:51 +0200

    scripts/02-init (build) dumb-init symlink

--- spec/alpine_server/init_spec.rb
+++ spec/alpine_server/init_spec.rb
@@ -5,7 +5,9 @@ describe 'init', :init do
     it { should be_file }
     it { should be_owned_by 'root' }
     it { should be_grouped_into 'root' }
-    it { should be_mode 755 }
+
+    it { should be_symlink }
+    it { should be_linked_to '/usr/bin/dumb-init' }
   end
 
   describe command('/sbin/init --version 2>&1') do
