38ebed4434b2bc5258b0c950b6f13b5bd68ed3c5
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-03-18 00:05:57 +0100

    log_spec (spec) amjor changes and improvements

--- spec/alpine_server/log_spec.rb
+++ spec/alpine_server/log_spec.rb
@@ -1,12 +1,31 @@
 # frozen_string_literal: true
 
 describe 'log files', :log do
-  ['/var/log/lastlog',
-   '/var/log/messages',
-   '/var/log/ylem'].each do |fp|
-    describe file(fp) do
-      it { should be_file }
-      it { should be_owned_by 'root' }
-    end
+  describe file('/var/log/messages') do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'syslog' }
+    it { should be_mode 644 }
+  end
+
+  describe file('/var/log/secure') do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    it { should be_mode 640 }
+  end
+
+  describe file('/var/log/ylem') do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    it { should be_mode 640 }
+  end
+
+  describe file('/var/log/lastlog') do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'utmp' }
+    it { should be_mode 644 }
   end
 end
