faa9cc61fa1e26c3a8d35203ae93c5910a42c01b
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-07-17 19:55:59 +0200

    home_spec (spec) ensure access 400 on root skel files

--- spec/alpine_server/home_spec.rb
+++ spec/alpine_server/home_spec.rb
@@ -39,6 +39,7 @@ describe 'home files', :home do
 
       it { should be_owned_by 'root' }
       it { should be_grouped_into 'root' }
+      it { should be_mode 400 }
     end
   end
   # @formatter:on
