261574cbba79cd82333640f7183e8d43cb2f2f08
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-07-19 09:00:01 +0200

    sbin_spec (spec) some examples added related to ylem

--- spec/alpine_server/sbin_spec.rb
+++ spec/alpine_server/sbin_spec.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-autoload(:Pathname, 'pathname')
-
 describe 'sbin files', :sbin do
   describe file('/sbin/runsvdir-start') do
     it { should be_file }
@@ -11,5 +9,14 @@ describe 'sbin files', :sbin do
     it { should be_mode 755 }
 
     its(:md5sum) { should eq '07fc5baddc59fb078bf4c9978c4741d7' }
+  end
+
+  describe file('/sbin/ylem') do
+    it { should be_file }
+
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+
+    its(:md5sum) { should eq '4e9e6318d79a1e3095989d649d653e67' }
   end
 end
