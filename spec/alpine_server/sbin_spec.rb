ef6da3788913f5c1291d7dadb9989fd623545dc3
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-10-08 08:25:52 +0200

    sbin_spec (spec) update md5 for runsvdir-start

--- spec/alpine_server/sbin_spec.rb
+++ spec/alpine_server/sbin_spec.rb
@@ -8,7 +8,7 @@ describe 'sbin files', :sbin do
     it { should be_grouped_into 'root' }
     it { should be_mode 755 }
 
-    its(:md5sum) { should eq '07fc5baddc59fb078bf4c9978c4741d7' }
+    its(:md5sum) { should eq '490a5b2b162606ab6c8ebf61b0e431a2' }
   end
 
   describe file('/sbin/ylem') do
