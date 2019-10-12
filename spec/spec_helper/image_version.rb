badd2c9aa79c125b4b9c67241dd5ba367d8192af
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-10-12 13:33:09 +0200

    spec_helper (spec) image_version freeze

--- spec/spec_helper/image_version.rb
+++ spec/spec_helper/image_version.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require_relative 'image'
-autoload(:Pathname, 'pathname')
 autoload(:YAML, 'yaml')
 
 # Current Image version.
@@ -9,6 +8,6 @@ autoload(:YAML, 'yaml')
 # @type [Hash{String => String|Fixnum}]
 IMAGE_VERSION = lambda do
   image.version.file.realpath.read.tap do |s|
-    return YAML.safe_load(s)
+    return YAML.safe_load(s).freeze
   end
 end.call
