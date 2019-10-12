67c499af4c77233ce2d28f4634c8e9b1fa1ec996
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-10-12 13:40:26 +0200

    spec_helper (spec) IMAGE_VERSION#to_s method introduced

--- spec/spec_helper/image_version.rb
+++ spec/spec_helper/image_version.rb
@@ -8,6 +8,10 @@ autoload(:YAML, 'yaml')
 # @type [Hash{String => String|Fixnum}]
 IMAGE_VERSION = lambda do
   image.version.file.realpath.read.tap do |s|
-    return YAML.safe_load(s).freeze
+    YAML.safe_load(s).tap do |version|
+      version.singleton_class.__send__(:define_method, :to_s) do
+        [version['major'], version['minor'], version['patch']].join('.').freeze
+      end
+    end.tap { |version| return version.freeze }
   end
 end.call
