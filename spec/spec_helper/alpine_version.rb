e1386371e60b47a8ad5e11ac5a2255754629087e
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-10-12 13:30:18 +0200

    spec_helper (spec) alpine_version minor changes

--- spec/spec_helper/alpine_version.rb
+++ spec/spec_helper/alpine_version.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
+autoload(:Pathname, 'pathname')
+
 # Current Alpine version, based on (generated) Dockerfile.
 #
 # @type [String]
-ALPINE_VERSION = proc do
+ALPINE_VERSION = lambda do
   # @formatter:off
   Pathname.new(Dir.pwd)
           .join('image/Dockerfile')
           .read.scan(/^FROM alpine:((?:[0-9]+\.){2}[0-9]+)$/)
           .fetch(0).fetch(0).freeze
   # @formatter:on
-end.call
+end.call.freeze
