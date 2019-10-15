9934ed19822fa051774bfbc76fbe53e8ec97eb26
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-10-15 19:39:29 +0200

    spec_helper (spec) file/be_empty refactor

--- spec/spec_helper/matcher/file/be_empty.rb
+++ spec/spec_helper/matcher/file/be_empty.rb
@@ -1,22 +1,24 @@
 # frozen_string_literal: true
 
 RSpec::Matchers.define :be_empty do
-  autoload(:Shellwords, 'shellwords')
-
   match do |file|
-    Serverspec::Type::File.tap do |type|
-      "expected #{file.class.to_s.split('::').last} #{file.name.inspect} to respond to `empty?`".tap do |msg|
-        raise msg unless file.is_a?(type)
+    [file.class.to_s.split('::').last, file.name].tap do |type, fname|
+      "expected #{type} #{fname.inspect} to respond to `empty?`".tap do |msg|
+        raise msg unless file.is_a?(Serverspec::Type::File)
       end
     end
 
+    autoload(:Shellwords, 'shellwords')
     # @formatter:off
     args = Shellwords.join({
       true => ['ls', '-A', file.name],
       false => ['test', '-s', file.name]
     }.fetch(file.directory?))
+
+    {
+      true => ->(cmd) { cmd.exit_status.zero? and cmd.stdout.empty? },
+      false => ->(cmd) { cmd.exit_status.zero? }
+    }.fetch(file.directory?).call(Serverspec::Type::Command.new(args))
     # @formatter:on
-    command = Serverspec::Type::Command.new(args)
-    file.directory? ? command.stdout.empty? : command.exit_status.zero?
   end
 end
