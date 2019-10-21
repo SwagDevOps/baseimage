db515774a0b246fafdc1b26df506b48938142ffa
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-10-21 19:28:59 +0200

    helper (spec) alpine_version support quoted value

--- spec/helper/alpine_version.rb
+++ spec/helper/alpine_version.rb
@@ -9,7 +9,7 @@ ALPINE_VERSION = lambda do
   # @formatter:off
   Pathname.new(Dir.pwd)
           .join('image/Dockerfile')
-          .read.scan(/^FROM alpine:((?:[0-9]+\.){2}[0-9]+)$/)
+          .read.scan(/^FROM ["]{0,1}alpine:((?:[0-9]+\.){2}[0-9]+)["]{0,1}$/)
           .fetch(0).fetch(0).freeze
   # @formatter:on
 end.call.freeze
