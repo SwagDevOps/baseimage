0ff0a2e82bb6acf82a9cfdf80c8c07113eb1ac5a
Dimitri Arrigoni <dimitri@arrigoni.me>
2020-12-18 21:48:27 +0100

    resources (rake) vendorer minor changes

--- rake/resources/vendorer.rb
+++ rake/resources/vendorer.rb
@@ -5,15 +5,15 @@ autoload(:YAML, 'yaml')
 
 # @type [Object] image
 # @type [Pathname] vendor
-# @type [Vendorer] self
 image.path.join('vendorer.yml').realpath.read.yield_self { |s| YAML.safe_load(s).sort.to_h }.tap do |config|
   if Object.const_defined?(:Vendorer) and self.is_a?(Vendorer)
+    # @see https://github.com/grosser/vendorer
+    # @type [Vendorer] self
     config.each do |k, v|
-      path = vendor.join(k.to_s)
-      args = v.reject { |arg| arg.is_a?(Hash) }
+      args = [vendor.join(k.to_s)].concat(v.reject { |arg| arg.is_a?(Hash) })
       kwargs = v.keep_if { |h| h.is_a?(Hash) }.last.transform_keys(&:to_sym)
 
-      self.public_send(:folder, *[path].concat(args), **kwargs)
+      self.public_send(:folder, *args, **kwargs)
     end
   end
 end
