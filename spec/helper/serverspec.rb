e4907a353ec5fb85c4f1b2153efa1738c420722b
Dimitri Arrigoni <dimitri@arrigoni.me>
2021-03-17 19:37:39 +0100

    helper (spec) serverspec minor changes and improvements

--- spec/helper/serverspec.rb
+++ spec/helper/serverspec.rb
@@ -15,9 +15,12 @@ end.ssh.wait.network.fetch(0).tap do |host|
   set :host, host
   set :disable_sudo, true
   set :path, '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
-  set :ssh_options, Net::SSH::Config.for(host).merge(
+  {
     user_known_hosts_file: '/dev/null',
     user: image.ssh.fetch(:user),
-    port: image.ssh.fetch(:port)
-  )
+    port: image.ssh.fetch(:port),
+    check_host_ip: false,
+  }.yield_self do |options|
+    set :ssh_options, Net::SSH::Config.for(host).merge(options)
+  end
 end
